439cec3b3abfd1d17f9de3c921a20415ecf0a68d
Nicklas W <Nicklas2002@hotmail.com>
2023-10-04 00:42:21 +0200

    Did html stuff

--- fourthingsplus/src/main/java/app/Main.java
+++ fourthingsplus/src/main/java/app/Main.java
@@ -2,11 +2,22 @@ package app;
 
 import app.config.ThymeleafConfig;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    private static Map<String, String> userMap = new HashMap<>();
+
     public static void main(String[] args)
     {
+
+        initUserMap();
+
+
         // Initializing Javalin and Jetty webserver
 
         Javalin app = Javalin.create(config -> {
@@ -17,5 +28,16 @@ public class Main {
         // Routing
 
         app.get("/", ctx ->  ctx.render("index.html"));
+        app.post("/login", ctx -> UserServices.login(ctx,userMap));
+
     }
+
+    private static void initUserMap() {
+        userMap.put("Nicklas","1234");
+        userMap.put("Lars","1234");
+        userMap.put("Søren","1234");
+        userMap.put("Emilie","1234");
+    }
+
+
 }
---
+++ fourthingsplus/src/main/java/app/UserServices.java
@@ -0,0 +1,20 @@
+package app;
+
+import io.javalin.http.Context;
+
+import java.util.Map;
+
+public class UserServices {
+    public static void login(Context ctx, Map<String, String> userMap){
+        String userName = ctx.formParam("username");
+        String password = ctx.formParam("password");
+
+        if(userMap.getOrDefault(userName, "092324328493").equals(password)){
+            ctx.render("tasks.html");
+        }else {
+            ctx.attribute("message","Shit didn't work");
+            ctx.render("index.html");
+        }
+
+    }
+}
